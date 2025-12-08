bdc032eed8880b1d12d3c3090a4d962868d1c8f7
Mónica Zamorano Sardón <mzamoranos@uoc.edu>
2025-12-08 20:28:00 +0100

    Cambios a SJ

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,11 +1,8 @@
 import tailwindcss from '@tailwindcss/postcss';
 
-import autoprefixer from 'autoprefixer';
-
-export default {
-    plugins: {
-        tailwindcss:{},
-       autoprefixer:{},
-    },
-  }
-    
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,14 @@
 export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
   "./public/index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
   extend: {
-    extend: {
-      colors: {
-        maroon: {
-          500: '#9fa3a9',  // Un gris medio
-          600: '#6a6e73',  // Más oscuro
-          700: '#393d42',  // Aún más oscuro
-        },
+    colors: {
+      maroon: {
+        500: '#9fa3a9', // Un gris medio
+        600: '#6a6e73', // Más oscuro
+        700: '#393d42', // Aún más oscuro
       },
     },
   },
